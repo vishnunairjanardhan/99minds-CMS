b1c3f7182bb19edfba82c4c454c7107b03440789
vishnunairjanardhan <vjnair2001@gmail.com>
2026-04-06 14:58:06 +0530

    chore: configure Netlify build settings and update TinaCMS environment variables for production deployment

--- tina/config.tsx
+++ tina/config.tsx
@@ -9,12 +9,11 @@ import Category from "./collection/category";
 
 const config = defineConfig({
   telemetry: 'disabled',
-  clientId: 'ec3f00d4-f48d-4caf-916e-d2fbb034bf05', //process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
-  branch: 'main',
-  //process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
-  //process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-  //process.env.HEAD!, // Netlify branch env
-  token: 'f490830f6b10c8c490e62bb713b391da274e2f5d',//process.env.TINA_TOKEN!,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
+  branch: process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
+    process.env.HEAD!, // Netlify branch env
+  token: process.env.TINA_TOKEN!,
   media: {
     // If you wanted cloudinary do this
     // loadCustomStore: async () => {
